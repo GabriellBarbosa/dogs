2e77d200819794ee76d7a7506141d0a09eb0f704
Gabriel Barbosa <bielbarbosa900@gmail.com>
2020-10-19 11:12:34 -0300

    Updates

---
+++ precache-manifest.ee86dd30ee13e4621a4c2eae37d92144.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba17f53869cd2e4f5c8e25bdeb9ca440",
+    "url": "/dogs/index.html"
+  },
+  {
+    "revision": "c4ab6a5ba1df5ca38886",
+    "url": "/dogs/static/css/4.4e6f6593.chunk.css"
+  },
+  {
+    "revision": "b17a18be822448c7299c",
+    "url": "/dogs/static/css/main.72cf1a23.chunk.css"
+  },
+  {
+    "revision": "22b1e3b803be1b7b64f5",
+    "url": "/dogs/static/js/2.89a1a869.chunk.js"
+  },
+  {
+    "revision": "63923b260a51337211b1",
+    "url": "/dogs/static/js/3.972412f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dogs/static/js/3.972412f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ab6a5ba1df5ca38886",
+    "url": "/dogs/static/js/4.6a4516ce.chunk.js"
+  },
+  {
+    "revision": "b17a18be822448c7299c",
+    "url": "/dogs/static/js/main.9a06b72b.chunk.js"
+  },
+  {
+    "revision": "a6066c3ba5434a2e69fd",
+    "url": "/dogs/static/js/runtime-main.2710955e.js"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/dogs/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "8cc31290254329d8d3990af08d454cd7",
+    "url": "/dogs/static/media/dogs-footer.8cc31290.svg"
+  },
+  {
+    "revision": "849a64433f3bdd08d38623156960aa56",
+    "url": "/dogs/static/media/dogs.849a6443.svg"
+  },
+  {
+    "revision": "26c6de43474fd0ef178cb3a9d26c4995",
+    "url": "/dogs/static/media/enviar.26c6de43.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/dogs/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/dogs/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "824d7d85ae3af9a43a61d46fe174192e",
+    "url": "/dogs/static/media/login.824d7d85.jpg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/dogs/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/dogs/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/dogs/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/dogs/static/media/visualizacao.4ce15ed8.svg"
+  }
+]);
